87cd52feea015e024dadad5ed95a09a68e261956
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-03-15 12:51:41 +0100

    lib implementation

--- include/libi2c.h
+++ include/libi2c.h
@@ -18,77 +18,135 @@
 #define ACK_VAL         (0x01)
 #define NACK_VAL        (0x00)
 
+#define WRITE_BIT       I2C_MASTER_WRITE
+#define READ_BIT        I2C_MASTER_READ
+
+#define NO_BUF          (0x00)
+
+#define PORT_0           I2C_NUM_0
+#define PORT_1           I2C_NUM_1
+
+#ifndef __cplusplus
+#define noop            (void)0
+#define assert(x)       ((!x || x <= 0) ? exit(1) : noop)
+#endif
+
+
 typedef uint8_t u8;
+typedef u8 i2c_buf_size_t;
+typedef u8 i2c_addr_t;
 
 /**
- * @struct i2c_config
- * @var i2c_config::esp_idf_conf
+ * @struct i2c_bus_t
+ * @var i2c_config::conf
  *  ESP-IDF i2c configuration struct
- * @var i2c_config is_master
- *  true if this device is the communication's master
- * @var i2c_config::is_slave
- *  true if this device is the communication's slave
+ * @var i2c_config::port
+ *  i2c port number
+ * @var i2c_config_t::rx
+ *  receive buffer size. Leave 0 for master. It doesn't need a buffer
+ * @var i2c_config_t::tx
+ *  transmission buffer size. Leave 0 for master. It doesn't need a buffer
  * @see i2c_config_t
+ * @see i2c_port_t
+ * @see i2c_buf_size_t
  */
-struct i2c_config {
-    i2c_config_t esp_idf_conf;
-    bool is_master;
-    bool is_slave;
+struct i2c_bus_t {
+    i2c_config_t conf;
+    i2c_port_t port;
+    i2c_buf_size_t rx;
+    i2c_buf_size_t tx;
 };
 
 /**
  * @brief i2c_config struct "constructor"
  */
-#define INIT_I2C_CONFIG(X) (struct i2c_config X = {
-    .esp_idf_conf = {
-        .sda_pullup_en = GPIO_PULLUP_ENABLE;
-        .scl_pullup_en = GPIO_PULLUP_ENABLE;
-    },
-    .is_master = false;
-    .is_slave = false;
-})
+//#define INIT_I2C_BUS_CONFIG_DEFAULT(X) struct i2c_bus_t X = { .conf = {.mode = I2C_MODE_MASTER; .sda_io_num = 21; .scl_io_num = 22; .sda_pullup_en = GPIO_PULLUP_ENABLE; .scl_pullup_en = GPIO_PULLUP_ENABLE; .master.clk_speed = 400000;  /* 400 kHz */}; .port = PORT_0; .rx = NO_BUF;  /* Leave 0 for master. It doesn't need a buffer */ .tx = NO_BUF;  /* Leave 0 for master. It doesn't need a buffer */ };
+#define INIT_I2C_BUS_CONFIG_DEFAULT(X) struct i2c_bus_t X = { \
+    .conf = { \
+        .mode = I2C_MODE_MASTER, \
+        .sda_io_num = 21, \
+        .scl_io_num = 22, \
+        .sda_pullup_en = GPIO_PULLUP_ENABLE, \
+        .scl_pullup_en = GPIO_PULLUP_ENABLE, \
+        .master.clk_speed = 400000,  /* 400 kHz */ \
+        }, \
+    .port = PORT_0, \
+    .rx = NO_BUF,  /* Leave 0 for master. It doesn't need a buffer */ \
+    .tx = NO_BUF,  /* Leave 0 for master. It doesn't need a buffer */ \
+    };
+
+/**
+ * @struct i2c_dev_handle_t
+ * @var i2c_dev_handle_t::port
+ *  i2c bus number
+ * @var i2c_dev_handle_t::addr
+ *  slave address (7-bit)
+ * @see i2c_port_t
+ * @see i2c_addr_t
+ */
+struct i2c_dev_handle_t {
+    i2c_port_t port;
+    i2c_addr_t addr;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /**
  * @brief initialize i2c communication
  * @param conf configuration struct
  * @return void
  */
-static void i2c_init(struct i2c_config *conf);
+void i2c_init(const struct i2c_bus_t *conf);
 
 /**
- * @brief read a series of len bytes and save them into an array
- * @param len number of bytes to read. Length of data array
+ * @brief read a series of len bytes and save them into an array. Only for master.
+ * @param dev pointer to dev handle structure
+ * @param size number of bytes to read. Length of data array
  * @param data pointer to an array of uint8_t, where the data will be stored
- * @return void
+ * @return error code
  */
-static void i2c_read_bytes(u8 len, u8 *data);
+esp_err_t i2c_read_bytes(const struct i2c_dev_handle_t *dev, u8 size, u8 *data);
 
 /**
  * @brief read one byte and return it
+ * @param dev pointer to dev handle structure
  * @return the read byte
  */
-static u8 i2c_read_byte();
+u8 i2c_read_byte(const struct i2c_dev_handle_t *dev);
 
 /**
  * @brief send a series of bytes to the slave
- * @param len data's length
+ * @param dev pointer to dev handle structure
+ * @param size data's length
  * @param data array of uint8_t
  * @return void
  */
-static void i2c_write_bytes(u8 len, u8 *data);
+esp_err_t i2c_write_bytes(const struct i2c_dev_handle_t *dev, u8 size, const u8 *data);
 
 /**
  * @brief send a byte to the slave
+ * @param dev pointer to dev handle structure
  * @param data byte to send
  * @return void
  */
-static void i2c_write_byte(u8 data);
+esp_err_t i2c_write_byte(const struct i2c_dev_handle_t *dev, u8 data);
 
 /**
  * @brief select a register. Make the register "pointer" on the slave points to reg
  * @param reg register's address on the slave
  * @return void
  */
-static void i2c_select_register(u8 reg);
+void i2c_select_register(u8 reg);
+
+/**
+ * @brief delete i2c driver and free memory
+ */
+void i2c_deinit(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif  // __LIBI2C_H
--- src/libi2c.c
+++ src/libi2c.c
@@ -7,6 +7,75 @@
 
 #include <libi2c.h>
 
-static void i2c_init(struct i2c_config *conf) {
+struct i2c_bus_t tmp_conf;
 
+/**
+ * @brief check if a pointer is null. Use in combination with assert()
+ * @param ptr pointer, input argument
+ * @return true if ptr is not null, false otherwise
+ */
+static bool ptr_check(void *ptr) {
+    return ptr != NULL;
+}
+
+// static void i2c_master_init(void) {  // configuration structure contained is tmp_conf
+    
+// }
+
+// static void i2c_slave_init(void) {  // configuration structure contained is tmp_conf
+
+// }
+
+void i2c_init(const struct i2c_bus_t *conf) {
+    tmp_conf = *conf;
+    // if (tmp_conf.esp_idf_conf.mode == I2C_MODE_MASTER) {
+    //     i2c_master_init();
+    // }
+    // else if (tmp_conf.esp_idf_conf.mode == I2C_MODE_SLAVE) {
+    //     i2c_slave_init();
+    // }
+    // else {  // Neither master nor slave are set. Or both master and slave are set to true.
+    //     return;
+    // }
+    i2c_param_config(tmp_conf.port, &(tmp_conf.conf));
+    i2c_driver_install(tmp_conf.port, tmp_conf.conf.mode, tmp_conf.rx, tmp_conf.tx, 0);
+}
+
+esp_err_t i2c_read_bytes(const struct i2c_dev_handle_t *dev, u8 size, u8 *data) {
+    assert(size);
+    assert(ptr_check(data));
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (dev->addr << 1) | READ_BIT, ACK_CHECK_EN);
+    if (size > 1) {
+        i2c_master_read(cmd, data, size - 1, ACK_CHECK_EN);
+    }
+    i2c_master_read_byte(cmd, data + size - 1, NACK_VAL);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(dev->port, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+u8 i2c_read_byte(const struct i2c_dev_handle_t *dev) {  // dev pointer integrity delegated to i2c_read_bytes
+    u8 *buf = (u8*) malloc(sizeof(u8) * 1);
+    i2c_read_bytes(dev, 1, buf);
+    return *buf;
+}
+
+esp_err_t i2c_write_bytes(const struct i2c_dev_handle_t *dev, u8 size, const u8 *data) {
+    assert(ptr_check(dev));
+    assert(size);
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (dev->addr << 1) | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write(cmd, data, size, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(dev->port, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+esp_err_t i2c_write_byte(const struct i2c_dev_handle_t *dev, u8 data) {  // pointer integrity check delegated to i2c_write_bytes
+    return i2c_write_bytes(dev, 1, &data);    
 }
