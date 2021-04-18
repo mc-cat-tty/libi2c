3a2a52b086eca7131e9069dbf98ab5f5fd4fc5b3
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-04-18 12:51:10 +0200

    update sensor name

--- examples/bmp280_id.c
+++ examples/bmp280_id.c
@@ -12,12 +12,12 @@ void app_main() {
     struct i2c_bus_t master_config = init_i2c_bus_default_master();
     master_config.conf.master.clk_speed = 100000;
     i2c_init(&master_config);
-    struct i2c_dev_handle_t bme280 = {.addr = 0x76, .port = PORT_1};
+    struct i2c_dev_handle_t bmp280 = {.addr = 0x76, .port = PORT_1};
     uint8_t id;
 
     while (true) {
-        i2c_select_register(&bme280, 0xd0);
-        i2c_read_bytes(&bme280, &id, 1);  
+        i2c_select_register(&bmp280, 0xd0);
+        i2c_read_bytes(&bmp280, &id, 1);  
         printf("Received bytes: 0x%02x\n", id);
         vTaskDelay(100/portTICK_RATE_MS);
     }
