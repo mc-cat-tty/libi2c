aaf5458fc639a47e7df941ceed6028d98c806277
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-04-18 12:30:39 +0200

    clean up code

--- examples/bmp280_id.c
+++ examples/bmp280_id.c
@@ -9,17 +9,16 @@
 #include <string.h>
 
 void app_main() {
-    // PORT_1
     struct i2c_bus_t master_config = init_i2c_bus_default_master();
     master_config.conf.master.clk_speed = 100000;
     i2c_init(&master_config);
     struct i2c_dev_handle_t bme280 = {.addr = 0x76, .port = PORT_1};
-    uint8_t rcv_buf[128];
+    uint8_t id;
 
     while (true) {
         i2c_select_register(&bme280, 0xd0);
-        i2c_read_bytes(&bme280, rcv_buf, 1);  
-        printf("Received bytes: 0x%02x\n", rcv_buf[0]);
+        i2c_read_bytes(&bme280, &id, 1);  
+        printf("Received bytes: 0x%02x\n", id);
         vTaskDelay(100/portTICK_RATE_MS);
     }
 }
